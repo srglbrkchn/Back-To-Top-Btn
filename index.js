0f2ce6ef896485f1bea21d61eefc48fe2e9c6e01
Sara <sara@wlan-e1424.fem.tu-ilmenau.de>
2022-04-09 22:40:26 +0200

    Bug fixed.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+
+// Getting the button
 topButton = document.getElementById("topBtn");
 
 
+// Calling the function when user scrolls down
 window.onscroll = function(){scrollDown()};
 
+// Function called when user scrolls down
 function scrollDown() {
   if (document.body.scrollTop > 45 || document.documentElement.scrollTop > 45) {
     topButton.style.display = "block";
@@ -11,8 +15,8 @@ function scrollDown() {
   }
 }
 
-
+// Functin called by button click
 function toTheTop() {
   document.documentElement.scrollTop = 0;
-  document.body.scrollTo = 0;
+  document.body.scrollTop = 0;
 }
